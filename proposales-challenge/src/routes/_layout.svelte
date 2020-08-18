909664000d0d0929dffd6e2ce5834c947d2aa827
ivancis <diazianluis@gmail.com>
2020-08-18 12:43:25 +0200

    card component init

--- proposales-challenge/src/routes/_layout.svelte
+++ proposales-challenge/src/routes/_layout.svelte
@@ -1,21 +1,18 @@
 <script>
-	import Nav from '../components/Nav.svelte';
+	// import Nav from '../components/Nav.svelte';
 
-	export let segment;
+	// export let segment;
 </script>
 
 <style>
 	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
+		display: grid;
+		min-height: 100vh;
+		border-top: 1em solid var(--proposales-dark);
 	}
 </style>
 
-<Nav {segment}/>
+<!-- <Nav {segment}/> -->
 
 <main>
 	<slot></slot>
--- proposales-challenge/src/routes/index.svelte
+++ proposales-challenge/src/routes/index.svelte
@@ -1,46 +1,43 @@
+<script>
+	import Card from '../components/Card.svelte';
+</script>
+
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+.page {
+	margin: 0 auto;
+	padding: 1em;
+	width: 100%;
+	max-width: var(--proposales-col-size);
+}
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Proposales : New proposal</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<div class="page">
+	<div class="page-title">
+		<h1>Create new proposal:</h1>
+		<h3>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et.</h3>
+	</div>
+	
+	<div class="page-content">
+		<ul class="card-list">
+			<li class="card-list__item">
+				<Card />
+			</li>
+			<li class="card-list__item">
+				<Card />
+			</li>
+			<li class="card-list__item">
+				<Card />
+			</li>
+			<li class="card-list__item">
+				<Card />
+			</li>
+		</ul>
+	</div>
 
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+	<div class="page-action">
+	</div>
+</div>
